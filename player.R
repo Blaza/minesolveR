2bef5b06042ea017288ea89dcad8f01c99becb72
Blaza Ivanovic <radebeka@gmail.com>
2017-07-18 22:22:12 +0200

    Update player.R

--- player.R
+++ player.R
@@ -10,20 +10,26 @@ best_move_gen <- function() {
     if (dim(move_stack)[1] == 0) {
       board <- read_board(im)
       # first try basic solving
-      move_stack <<- which(basic_solve(board, 10) == "n", arr.ind = TRUE)
-
+      board <- basic_solve(board, 10)
+      move_stack <<- which(board == "n", arr.ind = TRUE)
+      print("Basic solving")
       # if that gives no results, try contradiction solving too
-      if(dim(move_stack)[1] == 0)
-        move_stack <<- which(solve_board(board, 10) == "n", arr.ind = TRUE)
+      if(dim(move_stack)[1] == 0) {
+        print("Contradiction solving")
+        board <- solve_board(board, 10)
+        move_stack <<- which(board == "n", arr.ind = TRUE)
+      }
     }
 
     # if move_stack still empty, pick the first closed and hope for the best
-    if (dim(move_stack)[1] == 0)
-        move_stack <<- which(board == "z", arr.ind = TRUE)[1, ]
-
+    if (dim(move_stack)[1] == 0) {
+      print("Guessing")
+      move_stack <<- which(board == "z", arr.ind = TRUE)[1, , drop = FALSE]
+    }
+    
     # get the first element from the stack and remove it from the stack
-    next_move <<- move_stack[1, ]
-    move_stack <<- move_stack[-1, ]
+    next_move <<- move_stack[1, , drop = FALSE]
+    move_stack <<- move_stack[-1, , drop = FALSE]
 
     # return next move
     next_move
