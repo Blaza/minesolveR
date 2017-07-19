e5c76ad3b58daab0480748ec18abc1e74ce084a7
Blaza <radebeka@gmail.com>
2017-07-19 23:23:27 +0200

    Resize in classifier

--- classifier.R
+++ classifier.R
@@ -6,6 +6,7 @@ mines_model <- readRDS(paste(script.dir, 'mines_model.RDS', sep='/'))
 
 read_board <- function(im) {
   predictors <- c("x_arc_length", "y_arc_length")
+  im <- im %>% resize(780, 780)
 
   fields <- extract_fields(process_img(im), get_boundaries(decolor(im)))
   preds <- get_field_predictors(predictors, fields)
